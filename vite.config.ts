5926536320286890b1e13e9ed131ba59c2df2093
northmadd <kaptenahmad01@gmail.com>
2026-08-20 21:05:11 +0700

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/lontara-honey/",
   plugins: [react()],
   resolve: {
     alias: {
